ebbb034c32e09bcca1c6f54bd3b5b5ad2816d607
Dwikie <dwikie.rp@gmail.com>
2023-02-24 13:56:22 +0700

    Upd: - Update message when build and push image via script shell

--- build_push_image.sh
+++ build_push_image.sh
@@ -4,20 +4,21 @@ docker build -t item-app:v1 .
 docker images
 echo
 # Push Docker Hub
-echo "Push ke GitHub Container Registry/GitHub Packages"
+echo "Push image ke DockerHub..."
 # Mengubah nama image sesuai dengan nama user dan repository docker hub
 docker tag item-app:v1 neithroid/item-app:v1
 # Melakukan login docker
 echo $PASSWORD_DOCKER_HUB | docker login -u neithroid --password-stdin
 # Melakukan upload/push image ke docker hub
 docker push neithroid/item-app:v1
-echo "Sukses push ke repository Docker Hub"
+echo "Sukses push image ke repository Docker Hub"
 echo
 # Push Github Package
-echo "Push ke GitHub Container Registry/GitHub Packages"
+echo "Push image ke GitHub Container Registry/GitHub Packages..."
 # Mengubah nama image sesuai dengan nama user dan repository github
 docker tag item-app:v1 ghcr.io/dwikie/dicoding-microservices:proyek-pertama
 # Melakukan login docker with Personal Access Token Github
 echo $PAT | docker login ghcr.io -u dwikie --password-stdin
 # Melakukan upload/push ke github packages
 docker push ghcr.io/dwikie/dicoding-microservices:proyek-pertama
+echo "Sukses push image ke repository GitHub"
