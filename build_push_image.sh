a87d03cd498aa8020a3e54af28abfab331239e5c
Dwikie <dwikie.rp@gmail.com>
2023-02-24 10:13:12 +0700

    upd: Remove github workflows to build and publish image to github package and add script to push iamge to docker packages via docker cli

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,9 +2,18 @@
 docker build -t item-app:v1 .
 # Menampilan list image pada local docker
 docker images
+
+# Push Docker Hub
 # Mengubah nama image sesuai dengan nama user dan repository docker hub
 docker tag item-app:v1 neithroid/item-app:v1
 # Melakukan login docker
 echo $PASSWORD_DOCKER_HUB | docker login -u neithroid --password-stdin
 # Melakukan upload/push image ke docker hub
 docker push neithroid/item-app:v1
+
+# Push Github Package
+docker tag item-app:v1 ghcr.io/dwikie/dicoding-microservices:proyek-pertama
+# Melakukan login docker
+echo $TOKEN_GITHUB_PKG | docker login -u neithroid --password-stdin
+# Melakukan upload/push ke github packages
+docker push ghcr.io/dwikie/dicoding-microservices:proyek-pertama
